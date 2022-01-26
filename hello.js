86fab48745faf8e4527b6a60653544211c9bb0ed
Sudheer1415 <95935426+Sudheer1415@users.noreply.github.com>
2022-01-26 10:08:51 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,31 @@
+var a =10;
+var b = 20;
+var d = a-b;
+
+console.log(d);
+
+
+var a=16;
+var b=20;
+
+if(a>b){
+console.log("The greater the number is",a);
+}
+else{
+console.log("The greater number is",b);
+}
+
+
+
+var a=16;
+var b=20;
+var c=14;
+if(a>b && a>c){
+console.log("The greater the number is",a);
+}
+else if(b>a && b>c){
+console.log("The greater number is",b);
+}
+else{
+console.log("The greater number is",c);
+}
